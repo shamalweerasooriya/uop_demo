45ea1698361f6ac184d6b8b692bdaa858a577c9b
shamal <shamal.weerasooriya@pagero.com>
2026-03-19 11:06:36 +0530

    test pipeline

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,28 @@
-import { useTodos } from './hooks/useTodos.ts';
-import { TodoInput } from './components/TodoInput.tsx';
-import { TodoList } from './components/TodoList.tsx';
-import { FilterBar } from './components/FilterBar.tsx';
+import { useTodos } from "./hooks/useTodos.ts";
+import { TodoInput } from "./components/TodoInput.tsx";
+import { TodoList } from "./components/TodoList.tsx";
+import { FilterBar } from "./components/FilterBar.tsx";
 
 function App() {
-  const { todos, filter, activeCount, setFilter, addTodo, toggleTodo, deleteTodo } =
-    useTodos();
+  const {
+    todos,
+    filter,
+    activeCount,
+    setFilter,
+    addTodo,
+    toggleTodo,
+    deleteTodo,
+  } = useTodos();
 
   return (
     <div className="app">
-      <h1>Todo App</h1>
+      <h1>Todo App Demo</h1>
       <TodoInput onAdd={addTodo} />
-      <FilterBar current={filter} activeCount={activeCount} onChange={setFilter} />
+      <FilterBar
+        current={filter}
+        activeCount={activeCount}
+        onChange={setFilter}
+      />
       <TodoList todos={todos} onToggle={toggleTodo} onDelete={deleteTodo} />
     </div>
   );
