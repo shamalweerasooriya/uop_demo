3768a9ad8053fa108b8329375b2cd7250bbd1455
shamal <shamal.weerasooriya@pagero.com>
2026-03-20 10:48:26 +0530

    comittttttt

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
 
   return (
     <div className="app">
-      <h1>My AwesomeTodo App</h1>
+      <h1>My stupid app that no one wants to use</h1>
       <TodoInput onAdd={addTodo} />
       <FilterBar
         current={filter}
